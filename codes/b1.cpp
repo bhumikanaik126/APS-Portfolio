90fdd130020aade2566f22f82b5906b53552db21
Bhumika Naik <168616150+bhumikanaik126@users.noreply.github.com>
2025-06-07 12:02:16 +0530

    Create b1.cpp

---
+++ codes/b1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+// Function to compute LCS length
+int LCS(string a, string b) {
+    int m = a.length(), n = b.length();
+    vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+
+    // Build the LCS table
+    for (int i = 1; i <= m; i++) {
+        for (int j = 1; j <= n; j++) {
+            if (a[i - 1] == b[j - 1])
+                dp[i][j] = 1 + dp[i - 1][j - 1];
+            else
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+        }
+    }
+
+    return dp[m][n];
+}
+double computeSimilarity(string query, string title) {
+    int lcsLen = LCS(query, title);
+    return (double)lcsLen / query.length(); // Can also use avg length
+}
